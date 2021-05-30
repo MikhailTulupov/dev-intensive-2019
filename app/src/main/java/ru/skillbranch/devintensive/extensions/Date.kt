ad9d5746e48f9f1ee57c620efe619e1d02bf51d9
Mikhail <kd.maikl-201@yandex.ru>
2021-05-30 14:01:54 +0700

    add humanizeDiff fun

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.lang.Math.abs
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -20,6 +21,7 @@ fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
 }
+
 // возращает время отмотанное вперед или назад
 fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     var time = this.time
@@ -32,4 +34,17 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     }
     this.time = time
     return this
+}
+
+fun Date.humanizeDiff(date: Date = Date()): String =
+    when (val time = kotlin.math.abs(this.time - date.time)) {
+        in 0 * SECOND..1 * SECOND -> "только что"
+        in 1 * SECOND..45 * SECOND -> "несколько секунд назад"
+        in 45 * SECOND..75 * SECOND -> "минуту назад"
+        in 75 * SECOND..45 * MINUTE -> "${time / MINUTE} минут назад"
+        in 45 * MINUTE..75 * MINUTE -> "час назад"
+        in 75 * MINUTE..22 * HOUR -> "${time / HOUR} часов назад"
+        in 22 * HOUR..26 * HOUR -> "день назад"
+        in 26 * HOUR..360 * DAY -> "${time / DAY} дней назад"
+        else -> "более года назад"
 }
