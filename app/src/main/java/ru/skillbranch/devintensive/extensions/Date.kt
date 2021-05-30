80e544666ce5619b37a513a5733a77e14e77ccb4
Mikhail <kd.maikl-201@yandex.ru>
2021-05-30 13:34:37 +0700

    create new extension fun format

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,2 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
+import java.util.*
+
+// возращает в удобном формате дату с локалью ru
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
