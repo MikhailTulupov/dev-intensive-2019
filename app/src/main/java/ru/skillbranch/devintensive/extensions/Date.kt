5d5775887bc703607d23b0b28718750bf9eb7dc8
Mikhail <kd.maikl-201@yandex.ru>
2021-05-30 14:22:48 +0700

    add plural fun

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -48,3 +48,44 @@ fun Date.humanizeDiff(date: Date = Date()): String =
         in 26 * HOUR..360 * DAY -> "${time / DAY} дней назад"
         else -> "более года назад"
 }
+
+fun TimeUnits.plural(value: Int): String {
+
+    val time = if (value % 100 > 20) value % 10 else value % 100
+
+    return when (this) {
+        TimeUnits.SECOND -> {
+            when (time) {
+                1 -> "$value секунду"
+                in 2..4 -> "$value секунды"
+                in 5..20 -> "$value секунд"
+                else -> throw IllegalStateException("seconds exception")
+            }
+        }
+        TimeUnits.MINUTE -> {
+            when (time) {
+                1 -> "$value минуту"
+                in 2..4 -> "$value минуты"
+                in 5..20 -> "$value минут"
+                else -> throw IllegalStateException("minutes exception")
+            }
+        }
+        TimeUnits.HOUR -> {
+            when (time) {
+                1 -> "$value час"
+                in 2..4 -> "$value часа"
+                in 5..20 -> "$value часов"
+                else -> throw IllegalStateException("hours exception")
+            }
+        }
+        TimeUnits.DAY -> {
+            when (time) {
+                1 -> "$value день"
+                in 2..4 -> "$value дня"
+                in 5..20 -> "$value дней"
+                else -> throw IllegalStateException("days exception")
+
+            }
+        }
+    }
+}
