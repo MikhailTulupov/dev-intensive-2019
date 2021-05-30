e3e4427f568cc166b9aecff9a58fc64af48dcb95
Mikhail <kd.maikl-201@yandex.ru>
2021-05-30 13:30:13 +0700

    create new fun parseFullName

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,16 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
 
+        if (fullName?.contains(Regex("\\w")) != true)
+            return null to null
+
+        val parts: List<String> = fullName.split(" ")
+
+        val firstName = parts.getOrNull(0)
+        val lastName = parts.getOrNull(1)
+
+        return firstName to lastName
+    }
 }
