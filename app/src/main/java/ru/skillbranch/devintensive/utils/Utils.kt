e73b108530e8c89e63bdba87cff77aa48bc89828
Mikhail <kd.maikl-201@yandex.ru>
2021-05-30 13:59:57 +0700

    add a couple fun toInitials and transliteration. Add map Symbols for translate

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,6 +1,40 @@
 package ru.skillbranch.devintensive.utils
 
+import java.util.*
+import kotlin.collections.HashMap
+
 object Utils {
+
+    private var mapSymbols: HashMap<String, String> = HashMap()
+    private val a_zA_ZInterval = 65..122
+    private val ruA_ZInterval = 1040..1071
+    private val rua_zInterval = 1072..1103
+
+    init {
+        val listRuSymbols = listOf(
+            "а", "б", "в", "г", "д",
+            "е", "ё", "ж", "з", "и",
+            "й", "к", "л", "м", "н",
+            "о", "п", "р", "с", "т",
+            "у", "ф", "х", "ц", "ч",
+            "ш", "щ", "ъ", "ы", "ь",
+            "э", "ю", "я"
+        )
+        val listEnSymbols = listOf(
+            "a", "b", "v", "g", "d",
+            "e", "e", "zh", "z", "i",
+            "i", "k", "l", "m", "n",
+            "o", "p", "r", "s", "t",
+            "u", "f", "h", "c", "ch",
+            "sh", "sh'", "", "i", "",
+            "e", "yu", "ya"
+        )
+
+        for (i in listRuSymbols.indices) {
+            mapSymbols[listRuSymbols[i]] = listEnSymbols[i]
+        }
+    }
+
     fun parseFullName(fullName: String?): Pair<String?, String?> {
 
         if (fullName?.contains(Regex("\\w")) != true)
@@ -13,4 +47,39 @@ object Utils {
 
         return firstName to lastName
     }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        val sb = StringBuilder()
+
+        for (i in payload.indices) {
+
+            val char = payload[i]
+
+            sb.append(
+                when (char.code) {
+                    in a_zA_ZInterval -> char
+                    in ruA_ZInterval -> mapSymbols[char.lowercaseChar().toString()]?.uppercase(
+                        Locale.ROOT
+                    )
+                    in rua_zInterval -> mapSymbols[char.toString()]
+                    else -> if(char.isWhitespace())
+                        divider
+                    else
+                        char
+                }
+            )
+        }
+
+        return sb.toString()
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? =
+        if (firstName?.contains(Regex("\\w")) == true)
+            if (lastName?.contains(Regex("\\w")) == true)
+                firstName[0].uppercaseChar().toString() +
+                        lastName[0].uppercaseChar()
+            else
+                firstName[0].uppercaseChar().toString()
+        else
+            null
 }
