860ced3eea508719adcfb92f92c261023b7718e0
Mikhail <kd.maikl-201@yandex.ru>
2021-06-01 18:49:52 +0700

    add new func

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,11 +1,74 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+const val KEY_STATUS = "STATUS"
+const val KEY_QUESTION = "QUESTION"
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+    lateinit var benderObj: Bender
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send
+
+        val status = savedInstanceState?.getString(KEY_STATUS) ?: Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString(KEY_QUESTION) ?: Bender.Question.NAME.name
+        benderObj = Bender(
+            question = Bender.Question.valueOf(question),
+            status = Bender.Status.valueOf(status)
+        )
+        Log.d("M_MainActivity", "onCreate")
+
+        val (r, g, b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+
+        textTxt.text = benderObj.askQuestion()
+        sendBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.iv_send) {
+            val (phrase, color) = benderObj.listenAnswer(
+                messageEt.text.toString().lowercase(Locale.getDefault())
+            )
+            messageEt.setText("")
+            val (r, g, b) = color
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(KEY_STATUS, benderObj.status.name)
+        outState.putString(KEY_QUESTION, benderObj.question.name)
+        Log.d(
+            "M_MainActivity",
+            "onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}"
+        )
     }
 }
